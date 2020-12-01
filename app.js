9bc1201b7c51c5edbe5a48da43196fcf0eb34393
Litbelb-development <72626657+Litbelb-development@users.noreply.github.com>
2020-12-01 10:33:55 -0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("heading").innerHTML =
-    localStorage["title"] || "Heading"; //default text
-document.getElementById("texteditor").innerHTML = 
-    localStorage["Text"] || "Type some text!"; //default text
-setInterval(saveFn() {
-    //Autosave Function
-    localStorage["title"] = document.getElementById("heading").innerHTML;
-    localStorage["Text"] = document.getElementById("texteditor").innerHTML;
-}, 1000);
